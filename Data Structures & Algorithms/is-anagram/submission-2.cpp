3bb65e47e24433db171e988542cb7f852392eb62
shiva0105 <84321787+shiva0105@users.noreply.github.com>
2026-07-04 23:26:41 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp;
+
+        for(char& ch : s) mp[ch]++;
+        for(char& ch : t) mp[ch]--;
+        for(auto& p : mp) {
+            if(p.second != 0) return false;
+        }
+        return true;
+    }
+};
